54be3ab5ff90157d339296488a68b8f97fd2542b
DESKTOP- 73UID8E <you@example.comC:UsersapattAppDataRoamingnpm>
2021-03-20 10:24:59 +0100

    Added fading in/out and switching views.

--- src/main.js
+++ src/main.js
@@ -27,6 +27,7 @@ const connectHTMLElems = () => {
 const setupListeners = () => {
     viewElements.searchInput.addEventListener('keydown', onEnterSubmit);
     viewElements.searchButton.addEventListener('click', onClickSubmit);
+    viewElements.returnToSearchBtn.addEventListener('click', returnToSearch);
 }
 
 
@@ -37,13 +38,51 @@ const initializeApp = () => {
 
 const onEnterSubmit = event => {
     if (event.key === 'Enter') {
+        fadeInOut();
         let query = viewElements.searchInput.value;
         getWeatherByCity(query).then(data => {
             console.log(data);
+            switchView();
+            fadeInOut();
         });
     }
 };
 
-const onClickSubmit = () => {};
+const onClickSubmit = () => {
+        let query = viewElements.searchInput.value;
+        getWeatherByCity(query).then(data => {
+            console.log(data);
+            switchView();
+        });
+};
+
+const fadeInOut = () => {
+    if (viewElements.mainContainer.style.opacity === '1' || viewElements.mainContainer.style.opacity === '') {
+        viewElements.mainContainer.style.opacity = '0';
+    } else {
+        viewElements.mainContainer.style.opacity = '1';
+    }
+}
+
+
+
+const switchView = () => {
+    if (viewElements.weatherSearchView.style.display !== 'none') {
+        viewElements.weatherSearchView.style.display = 'none';
+        viewElements.weatherForecastView.style.display = 'block'; 
+    } else {
+        viewElements.weatherForecastView.style.display = 'none';
+        viewElements.weatherSearchView.style.display = 'flex';
+    }
+}
+
+const returnToSearch = () => {
+    fadeInOut()
+
+    setTimeout(() => {
+        switchView();
+        fadeInOut()
+    }, 500);
+}
 
 document.addEventListener('DOMContentLoaded', initializeApp)
